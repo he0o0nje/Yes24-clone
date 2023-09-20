7dbb16533f36e56e27a87026ac8640dbf1425774
he0o0nje <dudwogjswp@naver.com>
2023-09-20 11:58:32 +0900

    9-20

--- js/contentData.js
+++ js/contentData.js
@@ -59,6 +59,7 @@ $.ajax({
                       <span class="goods_pub">${NBData.publisher}</span>`;
 
       $(element).find(".imgbox a img").attr("src", imageUrl);
+      $(element).find(".goods_info .goods_tCmt em").text(NBData.contents);
       $(element).find(".goods_info .goods_name a").text(NBData.title);
       $(element).find(".goods_info .goods_pubGrp").append(temp_html);
       $(element).find(".goods_info .yes_b").text(NBData.price);
@@ -103,6 +104,7 @@ $.ajax({
     let imageUrl = MultiData.thumbnail;
 
     $(this).find(".book_img a img").attr("src", imageUrl);
+    $(this).find(".book_info .book_cmt").text(MultiData.contents);
     $(this).find(".book_info .book_name a").text(MultiData.title);
     $(this).find(".book_info .goods_auth").text(MultiData.authors);
     $(this).find(".book_info .goods_pub").text(MultiData.publisher);
@@ -169,6 +171,7 @@ $.ajax({
     let imageUrl = MultiData.thumbnail;
 
     $(this).find(".book_img a img").attr("src", imageUrl);
+    $(this).find(".book_info .book_cmt").text(MultiData.contents);
     $(this).find(".book_info .book_name a").text(MultiData.title);
     $(this).find(".book_info .yes_b").text(MultiData.sale_price);
   });
@@ -197,12 +200,39 @@ $.ajax({
   headers: { Authorization: "KakaoAK 34c497c35774425c1c37314d61072948" },
 }).done(function (data) {
   $("#M3BotRight").each(function (i) {
-    console.log(data);
     const M3BotData = data.documents[i];
     let imageUrl = M3BotData.thumbnail;
 
     $(this).find(".book_img a img").attr("src", imageUrl);
+    $(this).find(".book_info .book_cmt").text(M3BotData.contents);
     $(this).find(".book_info .book_name a").text(M3BotData.title);
     $(this).find(".book_info .yes_b").text(M3BotData.sale_price);
+  });
+});
+
+// Multi04
+$.ajax({
+  method: "GET",
+  url: "https://dapi.kakao.com/v3/search/book?target=title",
+  data: { query: "집착광공" },
+  headers: { Authorization: "KakaoAK 34c497c35774425c1c37314d61072948" },
+}).done(function (data) {
+  console.log(data);
+  $("#MTS4 .bookBigCon").each(function (i) {
+    const MultiData = data.documents[i];
+    let imageUrl = MultiData.thumbnail;
+    let temp_html = `${MultiData.authors}
+    <em class="goods_divi">|</em>
+    ${MultiData.publishers}
+    <em class="goods_divi">|</em>
+    5화 무료
+    <em class="goods_divi">|</em>
+    ${MultiData.price}원`;
+
+    $(this).find(".book_img a img").attr("src", imageUrl);
+    $(this).find(".book_info .book_name a").text(MultiData.title);
+    $(this).find(".book_info .book_pubGrp").append(temp_html);
+    $(this).find(".book_info .book_readTit").text(MultiData.status);
+    $(this).find(".book_info .book_readCon").text(MultiData.contents);
   });
 });
