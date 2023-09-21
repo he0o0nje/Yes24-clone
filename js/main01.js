1472a48258db76a9dfbb65512313a5de4be6d807
he0o0nje <dudwogjswp@naver.com>
2023-09-21 13:56:10 +0900

    9-15(1)

--- js/main01.js
+++ js/main01.js
@@ -1,6 +1,6 @@
 // Main1
 
-// 메인배너 리스트 호버 시 css변경
+// 메인배너 리스트 호버 시 css변경, 해당 컨텐츠 표시
 $(".mbnTit").mouseenter(function () {
   $(".mbnTit").css({
     color: "",
@@ -27,3 +27,85 @@ $(".tabs").mouseenter(function () {
   $(".mbnContImg").hide();
   $(this).siblings(".mbnContImg").show();
 });
+
+// 메인배너 슬라이드
+$(function () {
+  $(".mbnContImg.active").show();
+
+  $(".mbnBtn.prev").click(function () {
+    MBNprevSlide();
+  });
+
+  $(".mbnBtn.next").click(function () {
+    MBNnextSlide();
+  });
+});
+
+// $(".mbnContImg").not(".active").hide();
+
+function MBNprevSlide() {
+  $(".mbnContImg").hide();
+  let allSlide = $(".mbnContImg");
+  let currentIndex = 0;
+
+  allSlide.each(function (index) {
+    if ($(this).hasClass("active")) {
+      currentIndex = index;
+    }
+  });
+
+  let newIndex = 0;
+
+  if (currentIndex <= 0) {
+    newIndex = allSlide.length - 1;
+  } else {
+    newIndex = currentIndex - 1;
+  }
+
+  $(".mbnContImg").removeClass("active");
+  $(".mbnContImg").eq(newIndex).addClass("active");
+  $(".mbnContImg").eq(newIndex).show();
+}
+
+function MBNnextSlide() {
+  $(".mbnContImg").hide();
+  let allSlide = $(".mbnContImg");
+  let currentIndex = 0;
+
+  allSlide.each(function (index) {
+    if ($(this).hasClass("active")) {
+      currentIndex = index;
+    }
+  });
+
+  let newIndex = 0;
+
+  if (currentIndex >= allSlide.length - 1) {
+    newIndex = 0;
+  } else {
+    newIndex = currentIndex + 1;
+  }
+
+  $(".mbnContImg").removeClass("active");
+  $(".mbnContImg").eq(newIndex).addClass("active");
+  $(".mbnContImg").eq(newIndex).show();
+}
+
+function YesNowslide() {
+  let current = $(".mbnContImg:visible");
+  let next = current.next(".mbnContImg");
+  if (next.length === 0) {
+    next = $(".mbnContImg:first");
+  }
+  current.hide();
+  next.show();
+
+  let index = next.index();
+  $(".tabs").css({ "background-color": "#fff", color: "#333" });
+  $(".tabs:eq(" + index + ")").css({
+    "background-color": "#333",
+    color: "#fff",
+  });
+}
+
+setInterval(YesNowslide, 3000);
